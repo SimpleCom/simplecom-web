1dfea7aecf032eb2a7525cfbf5af1302015f8600
Josh Terrill <joshterrill.dev@gmail.com>
2017-10-21 15:13:31 -0700

    refactored rest service and added json file and new lib

--- src/common/rest.service.ts
+++ src/common/rest.service.ts
@@ -31,21 +31,6 @@ export class RestService {
   }
 
   /**
-   * Abstracts HTTP POST
-   * @param url {string}
-   * @param body {any}
-   * @returns {Promise<Observable<any>|T>}
-   */
-  post2(url: string, body: any): Promise<any> {
-    return this._http.post(url, body, this.buildOptions2())
-      .map(res => res.json())
-      .toPromise()
-      .catch(err => {
-        this.handleError(err);
-      });
-  }
-
-  /**
    * Abstracts HTTP GET
    * @param url {string}
    * @returns {Promise<Observable<any>|T>}
@@ -60,40 +45,12 @@ export class RestService {
   }
 
   /**
-   * Abstracts HTTP GET
-   * @param url {string}
-   * @returns {Promise<Observable<any>|T>}
-   */
-  get2(url: string): Promise<any> {
-    return this._http.get(url, this.buildOptions2())
-      .map(res => res.json())
-      .toPromise()
-      .catch(err => {
-        this.handleError(err);
-      });
-  }
-
-  /**
    * Abstracts HTTP DELETE
    * @param url {string}
    * @returns {Promise<Observable<any>|T>}
    */
   delete(url: string): Promise<any> {
     return this._http.delete(url, this.buildOptions())
-      .map(res => res.json())
-      .toPromise()
-      .catch(err => {
-        this.handleError(err);
-      });
-  }
-
-  /**
-   * Abstracts HTTP DELETE
-   * @param url {string}
-   * @returns {Promise<Observable<any>|T>}
-   */
-  delete2(url: string): Promise<any> {
-    return this._http.delete(url, this.buildOptions2())
       .map(res => res.json())
       .toPromise()
       .catch(err => {
@@ -117,39 +74,16 @@ export class RestService {
   }
 
   /**
-   * Abstracts HTTP PUT
-   * @param url {string}
-   * @param body {any}
-   * @returns {Promise<Observable<any>|T>}
-   */
-  put2(url: string, body: any): Promise<any> {
-    return this._http.put(url, body, this.buildOptions2())
-      .map(res => res.json())
-      .toPromise()
-      .catch(err => {
-        this.handleError(err);
-      });
-  }
-
-  /**
    * Lets use set build options with auth token header on abstracted HTTP calls
    * @returns {{headers: Headers}} {RequestOptionsArgs}
    */
+
   buildOptions(): RequestOptionsArgs {
     const options = new RequestOptions();
     options.headers = new Headers();
     options.headers.append('Accept', 'application/json');
-    options.headers.append('Authorization', this.token);
-    console.log('this is token from bo1', this.token);
-    return options;
-  }
-
-  buildOptions2(): RequestOptionsArgs {
-    const options = new RequestOptions();
-    options.headers = new Headers();
-    options.headers.append('Accept', 'application/json');
     options.headers.append('Authorization', 'Bearer ' + this.token);
-    console.log('this is token from bo2', 'Bearer ' + this.token);
+    // console.log('this is token from bo2', 'Bearer ' + this.token);
     return options;
   }
 
