8af90f4820895ed0ad41056c1f8de0f237b697ae
TaylorLaMar <taylorlamar147@gmail.com>
2017-10-20 22:09:05 -0700

    Some stuff got added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,7 +9,58 @@ import { ActivatedRoute } from '@angular/router';
 export class HomeComponent implements OnInit {
   constructor(private route: ActivatedRoute) {}
 
+  private lists: any = [
+    {
+      "id": 1,
+      "name": "My First Group",
+      "people": [
+        {
+          "id": 1,
+          "name": "Person 1"
+        },
+        {
+          "id": 2,
+          "name": "Person 2"
+        },
+        {
+          "id": 3,
+          "name": "Person 3"
+        },
+        {
+          "id": 4,
+          "name": "Person 4"
+        }
+      ]
+    },
+    {
+      "id": 2,
+      "name": "My Second Group",
+      "people": [
+        {
+          "id": 5,
+          "name": "Person 5"
+        },
+        {
+          "id": 6,
+          "name": "Person 6"
+        },
+        {
+          "id": 7,
+          "name": "Person 7"
+        },
+        {
+          "id": 8,
+          "name": "Person 8"
+        }
+      ]
+    }
+  ];
+
   ngOnInit() {
-    
+    console.log(this.lists);
+  }
+
+  something(index) {
+    console.log(index);
   }
 }
